e3959476ab8b066c460ef65de8088441b72f7bf4
harishankaran01 <ghari3004@gmail.com>
2024-04-19 20:53:37 +0530

    Login and Register Completed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,9 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 const dbConnenction=async()=>{
 try{
-    const connect=await mongoose.connect(process.env.CONNECTION_STRING, {
+    const connect=await mongoose.connect(process.env.CONNECTION_STRING, 
+        //CONNECTION_STRING=mongodb+srv://Hari:hari3004@cluster0.k7rewxf.mongodb.net/FoodSystem?retryWrites=true&w=majority&appName=Cluster0
+        {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
